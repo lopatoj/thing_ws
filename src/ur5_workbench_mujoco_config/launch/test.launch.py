557b0ac7399dad87c0b4983a0d62f41df112e68f
Justin Lopato <justin@lopa.to>
2026-04-07 20:19:13 +0000

    FT Compensation attempt

--- src/ur5_workbench_mujoco_config/launch/test.launch.py
+++ src/ur5_workbench_mujoco_config/launch/test.launch.py
@@ -1,23 +1,36 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, Shutdown
-from launch.substitutions import PathJoinSubstitution
+from launch.actions import Shutdown
+from launch.substitutions import Command, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterFile, ParameterValue
 from launch_ros.substitutions import FindPackageShare
-from launch_ros.parameter_descriptions import ParameterFile
 
 
 def generate_launch_description():
     mujoco_config_package = FindPackageShare("ur5_workbench_mujoco_config")
-    description_package = FindPackageShare("ur5_workbench_description")
     urdf_file = PathJoinSubstitution(
         [mujoco_config_package, "urdf", "ur5_workbench.mujoco_ros2_control.xacro"]
     )
 
-    robot_state_publisher_launch = IncludeLaunchDescription(
-        PathJoinSubstitution(
-            [description_package, "launch", "robot_state_publisher.launch.py"]
-        ),
-        launch_arguments={"urdf_file": urdf_file}.items(),
+    robot_description_content = Command(
+        [
+            "xacro ",
+            urdf_file,
+        ]
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": ParameterValue(robot_description_content, value_type=str)}],
+    )
+
+    mjcf_conversion_node = Node(
+        package="mujoco_ros2_control",
+        executable="robot_description_to_mjcf.sh",
+        output="both",
+        emulate_tty=True,
+        arguments=["--publish_topic", "/mujoco_robot_description"],
     )
 
     parameters_file = PathJoinSubstitution(
@@ -26,7 +39,7 @@ def generate_launch_description():
     mujoco_plugins_file = PathJoinSubstitution(
         [mujoco_config_package, "config", "plugins.yaml"]
     )
-
+    rviz_config_file = PathJoinSubstitution([mujoco_config_package, "rviz", "wrench.rviz"])
 
     mujoco_ros2_control_node = Node(
         package="mujoco_ros2_control",
@@ -38,22 +51,70 @@ def generate_launch_description():
             ParameterFile(parameters_file),
             ParameterFile(mujoco_plugins_file),
         ],
-        on_exit=Shutdown()
+        on_exit=Shutdown(),
     )
     joint_state_broadcaster = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager", "--param-file", parameters_file],
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+            "--param-file",
+            parameters_file,
+        ],
     )
     position_controller = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["position_controller", "--controller-manager", "/controller_manager", "--param-file", parameters_file],
+        arguments=[
+            "position_controller",
+            "--controller-manager",
+            "/controller_manager",
+            "--param-file",
+            parameters_file,
+        ],
+    )
+    fts_broadcaster1 = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "force_torque_sensor_broadcaster1",
+            "--controller-manager",
+            "/controller_manager",
+            "--param-file",
+            parameters_file,
+        ],
+    )
+    fts_broadcaster2 = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "force_torque_sensor_broadcaster2",
+            "--controller-manager",
+            "/controller_manager",
+            "--param-file",
+            parameters_file,
+        ],
     )
 
-    return LaunchDescription([
-        robot_state_publisher_launch,
-        mujoco_ros2_control_node,
-        joint_state_broadcaster,
-        position_controller,
-    ])
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", rviz_config_file],
+    )
+
+    return LaunchDescription(
+        [
+            mjcf_conversion_node,
+            robot_state_publisher_node,
+            mujoco_ros2_control_node,
+            joint_state_broadcaster,
+            position_controller,
+            fts_broadcaster1,
+            fts_broadcaster2,
+            rviz_node
+        ]
+    )
